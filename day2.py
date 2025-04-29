372efb8035d892a377c9f9a3dc40432d8cffc13f
Pikelsss <jamespont678@gmail.com>
2025-04-29 15:13:05 +0800

    This is day 2 of learning python and we created different types of variables

---
+++ day2.py
@@ -0,0 +1,100 @@
+# Lets do the integer first.
+# What is Integer?
+# Integer is a whole number, positive or negative, or zero.
+# Where can we use this?
+# We can use it to count things, like the number of students in a class.
+# For example
+age = 25
+print("Integer:", age)
+# Result: Integer: 25
+
+# Next is Float
+# What is Float?
+# Float is a number that has a decimal point.
+# Where can we use this?
+# We can use it to measure things, like the height of a person.
+# For example
+height = 2.5
+print("Float:", height)
+# Result: Float: 2.5
+
+# Next is String
+# What is String?
+# String is a sequence of characters.
+# Where can we use this?
+# We can use it to store names, addresses, or any text.
+# For example
+name = "James"
+print("String:", name)
+# Result: String: James
+
+# Next is Boolean
+# What is Boolean?
+# Boolean is a data type that can have one of two values: True or False.
+# Where can we use this?
+# We can use it to check if something is true or false, like if a student is enrolled in a course.
+# For example
+is_student = True
+print("Boolean:", is_student)
+# Result: Boolean: True
+# What about false in boolean?
+# Where can we use this?
+# We can use it to check if something is false, like if a student is not enrolled in a course.
+# For example
+is_student = False
+print("Boolean:", is_student)
+# Result: Boolean: False
+
+# Next is List
+# What is List?
+# List is a collection of items.
+# Where can we use this?
+# We can use it to store multiple values, like a list of students in a class.
+# For example
+students = ["James", "Kaizer", "Mc"]
+print("List:", students)
+# Result: List: ['James', 'Kaizer', 'Mc']
+
+# Next is Tuple
+# What is Tuple?
+# Tuple is a collection of items that cannot be changed.
+# Where can we use this?
+# We can use it to store multiple values that should not be changed, like the coordinates of a point.
+# For example
+coordinates = (10, 20)
+print("Tuple:", coordinates)
+# Result: Tuple: (10, 20)
+
+# Next is Dictionary
+# What is Dictionary?
+# Dictionary is a collection of key-value pairs.
+# Where can we use this?
+# We can use it to store data in a structured way, like a phone book.
+# For example
+phone_book = {"James": "123-456-7890", "Kaizer": "987-654-3210", "Mc": "555-555-5555"}
+print("Dictionary:", phone_book)
+# Result: Dictionary: {'James': '123-456-7890', 'Kaizer': '987-654-3210', 'Mc': '555-555-5555'}
+# What about empty dictionary?
+# Where can we use this?
+# We can use it to store data in a structured way, like a phone book.
+# For example
+empty_dict = {}
+print("Empty Dictionary:", empty_dict)
+# Result: Empty Dictionary: {}
+
+# Next is Set
+# What is Set?
+# Set is a collection of unique items.
+# Where can we use this?
+# We can use it to store unique values, like a list of unique students in a class.
+# For example
+unique_students = {"James", "Kaizer", "Mc", "James"}
+print("Set:", unique_students)
+# Result: Set: {'James', 'Kaizer', 'Mc'}
+# What about empty set?
+# Where can we use this?
+# We can use it to store unique values, like a list of unique students in a class.
+# For example
+empty_set = set()
+print("Empty Set:", empty_set)
+# Result: Empty Set: set()
