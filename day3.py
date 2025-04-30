37080cb3a146fd380eb3e5b8f281e0cf32430b87
Pikelsss <jamespont678@gmail.com>
2025-04-30 21:26:26 +0800

    this is day 3 of learning python

---
+++ day3.py
@@ -0,0 +1,51 @@
+# lets define two numbers to do some basic math in python
+a = 5
+b = 10
+
+# Addition
+# what does it do?
+# it adds two numbers
+# what is the result?
+# it is 15
+
+sum_result = a + b
+print("Addition", sum_result)
+
+# Subtraction
+# what does it do?
+# it subtracts two numbers
+# what is the result?
+# it is -5
+difference = a - b
+print("Subtraction", difference)
+# you can also use sub_result = a - b
+# but it is not recommended
+# because it is not clear what it does
+# it is better to use difference = a - b
+# it is more clear what it does
+
+# Multiplication
+# what does it do?
+# it multiplies two numbers
+# what is the result?
+# it is 50
+product = a * b
+print("Multiplication", product)
+# you can also use prod_result = a * b
+# but it is not recommended
+# because it is not clear what it does
+# it is better to use product = a * b
+# it is more clear what it does
+
+# Division
+# what does it do?
+# it divides two numbers
+# what is the result?
+# it is 0.5
+quotient = a / b
+print("Division", quotient)
+# you can also use div_result = a / b
+# but it is not recommended
+# because it is not clear what it does
+# it is better to use quotient = a / b
+# it is more clear what it does
